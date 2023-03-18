012a755ba2b396d40fa3bec5e4ced8cf9a509dfa
Gabriel Vega <vegabito1112@gmail.com>
2023-03-18 19:29:14 -0400

    added functionality for a client to establish connection and then register itself

--- client.py
+++ client.py
@@ -1,21 +1,54 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
 class Client:
-    def __init__(self, ip, port):
+    #constructor for the client class 
+    def __init__(self, ip, port, name):
         self.ip = ip
         self.port = port
+        self.name = name
+        self.sock = socket(AF_INET, SOCK_STREAM)
 
-    def send(self, message):
-        sock = socket(AF_INET, SOCK_STREAM)
-        sock.connect((self.ip, self.port))
+    #this function establishes connection to the server
+    def establishConnection(self):
+        self.sock.connect((self.ip, self.port))
+        self.send("establish connection", "recieved establish connection request")
+        self.sock.send(self.name.encode())
+        retMsg = self.sock.recv(1024).decode()
+        match retMsg:
+            case "connection successful":
+                return retMsg
+            case "need to register":
+                self.register()
+                #attempts to connect again after registering
+                self.send("establish connection", "recieved establish connection request")
+                self.send(self.name, "connection successful")
+            case _:
+                return "error with connecting to server"
 
-        received = sock.recv(1024)
-        print(received.decode())
-        sock.send(message)
+    #this function sends the stuff nessisary to register itself with the server
+    def register(self):
+        self.send("register client", "recieved register client request")
+        self.send(self.name, f"registered {self.name} with server")
 
-        sock.close()
-
+    #this function requests all clients connected to the server 
+    def fetchClients(self):
+        while True:
+            self.sock.send(b"fetch clients")        
+    
+    #this function sends a message to the server and raises an error if it does not reciece the expected return message
+    def send(self, msg, expectedRetMsg):
+        self.sock.send(msg.encode())
+        retMsg = self.sock.recv(1024).decode()
+        if retMsg != expectedRetMsg:
+            raise Exception(f"error sending msg: {msg} to client: {self.ip} with retMsg: {retMsg}")
+    
+    #deconstructor for the client class sends message to the server to mark as inactive 
+    def __del__(self):
+        self.send("close connection", "recieved request to close connection")
+        self.sock.send(self.name.encode())
+        self.sock.close()
 
 if __name__ == '__main__':
-    client = Client('10.0.0.182', 9999)
-    client.send(b'Hello Server.')
+    client = Client('10.0.0.182', 9999, "test")
+    client.establishConnection()
+
--- network.py
+++ network.py
@@ -3,27 +3,76 @@ from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM
 import threading
 
 class Server:
+    #constructor for the server, sets up the ip, port and active and registered clients
     def __init__(self, port):
         s = socket(AF_INET, SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         
         self.ip = s.getsockname()[0]
         self.port = port
-        self.clients = []
+        self.activeClients = []
+        self.registeredClients = []
     
+    #makes the server start listening for clients
     def start(self):
+        #creates and binds the socket
         sock = socket(AF_INET, SOCK_STREAM)
         sock.bind(("0.0.0.0", self.port))
+        #prints out which ip address the server is running on 
+        print(f"Network started at address {self.ip}")
+        #start listening and creates a new thread to handle each connection
         sock.listen(10)
-
         while True:
             (clientSock, clientAddr) = sock.accept()
             t1 = threading.Thread(target = self.serviceClient, args=(clientSock, clientAddr)).start()
 
+    #This is the function that handles each connected client
     def serviceClient(self, sock, addr):
-        sock.send(b"connection successful")
-        msg = sock.recv(1024) 
-        print(msg.decode())
+        while True:
+            #this listens for a message to be sent from the client and then switches over the message to detect which operation the client is requesting
+            op = sock.recv(1024).decode()
+            print(f"Client {addr}: {op}")
+            match op:
+                #this is hit when the client attempts to connect to the server
+                case "establish connection":
+                    sock.send(b"recieved establish connection request")
+                    name = sock.recv(1024).decode()
+                    if name in self.registeredClients:
+                        if name not in self.activeClients:
+                            self.activeClients.append(name)
+                            sock.send(b"connection successful")
+                        else:
+                            sock.send(f"Client with the name {name} is already connected".encode())
+                            sock.close()
+                            exit()
+                    else:
+                        sock.send(b"need to register")
+                        self.registerClient(sock, addr)
+                #this is hit when the client requests all other connected clients
+                case "fetch clients":
+                    print("fetch clients")
+                #this gets hit when the client requests to close the connection
+                case "close connection":
+                    sock.send(b"recieved request to close connection")
+                    rem = sock.recv(1024).decode()
+                    self.activeClients.remove(rem)
+                    sock.close()
+                    exit()
+                case _:
+                    print("server recieved an invalid request")
+    
+    #this function preforms the logic for registering a client
+    def registerClient(self, sock, addr):
+        op = sock.recv(1024).decode()
+        if op == "register client":
+            sock.send(b"recieved register client request")
+            name = sock.recv(1024).decode()
+            self.registeredClients.append(name)
+            sock.send(f"registered {name} with server".encode())
+        else:
+            sock.send("failed to register closing connection")
+            sock.close()
+            exit()
 
 if __name__ == "__main__":
     server = Server(9999)
